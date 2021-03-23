de2ff3a502eb575036f0a368e91f2467cf0e9a08
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2021-03-24 02:17:21 +0530

    wallpaper

---
+++ app.js
@@ -0,0 +1,18 @@
+var options = {
+    strings: [
+        "a Human",
+        "Priyanshu",
+        "a Developer",
+        "a coder",
+        "an engineer",
+    ],
+    showCursor: true,
+    cursorChar: "...",
+    autoInsertCss: true,
+    typeSpeed: 40,
+    fadeOut: false,
+    backDelay: 1500,
+    loop: true,
+};
+
+var typed = new Typed(".element", options);
